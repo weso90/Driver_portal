f0a19b14cb0377d849449f0103ca33d53687f4b0
Maksym Wesolowski <weso.pl@gmail.com>
2025-04-26 20:45:01 +0200

    Added session

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "secret_key"
 
-@app.route('/')
-def index():
-    return "Witaj, Świecie!"
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Otwieranie sesji użytkownika
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'admin' and password == '123':
+            session['loged'] = True
+            session['username'] = username
+            return "Zalogowano pomyślnie"
+        else:
+            "Błąd logowania"
+    else:
+        # Wyświetl formularz logowania
+        return render_template('login.html')
+
+@app.route('/protected')
+def protected():
+    if 'loged' in session:
+        return f"Ta strona jest chroniona. Witaj, {session['username']}"
+    else:
+        return "Dostęp zabroniony. Zaloguj się"
+    
+@app.route('/logout')
+def logout():
+    session.pop('loged', None)
+    session.pop('username', None)
+    return "Wylogowano"
 
 if __name__ == "__main__":
     app.run(debug=True)
